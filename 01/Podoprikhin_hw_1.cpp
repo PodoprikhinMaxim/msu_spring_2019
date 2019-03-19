208cd1c703fbcfc2734046a67caddf5a0dabbd54
Maxim Podoprikhin <podoprikhin.maxim@gmail.com>
2019-03-19 16:57:45 +0300

    update hw_01

--- 01/Podoprikhin_hw_1.cpp
+++ 01/Podoprikhin_hw_1.cpp
@@ -3,61 +3,64 @@
 #include <vector>
 #include <math.h>
 
-int main(int argc, char* argv[])
+const int n = 100001;
+
+std::vector<int> sieve_Er() 
 {
-	std::vector<int> V;
-	for (int i = 1; i < argc; ++i)
-	{
-		int v = std::atoi(argv[i]);
-		V.push_back(v);
-	}
-	if(argv[1] == NULL)
-	{
-		return -1;
-	}
-	if(V.size()%2 != 0)
-		return -1;
-	else
-	{
-		std::vector<int> a;
-		for (int i = 0; i < 100001; i++)
+	std::vector<int> sieve (n + 1, 1);
+	sieve[0] = 0;
+	sieve[1] = 0;
+	for (int i = 2; i * i < n + 1; i++)
+		if (sieve[i])
 		{
-			a.push_back(1);
-		}
-		for(int i = 2; i*i <= 100001; i++)
-		{
-			if(a[i])
+			if (i * i <= n)
 			{
-				for(int j = i*i; j < 100001; j += i)
+				for (int j = i * i; j < n + 1; j += i)
 				{
-					a[j] = 0;
+					sieve[j] = 0;
 				}
 			}
 		}
-		a[0] = 0;
-		a[1] = 0;
-		for (int i = 0; i < V.size()-1; i += 2)
+	return sieve;
+};
+
+void prime_count(std::vector<int>& S, int start, int end) 
+{
+	int count = 0;
+	int j = 0;
+	while(Data[j] < start)
+	{
+		j++;
+	}
+	if(Data[j] != start)
+	{
+		j++;
+	}
+	for(j; Data[j] <= end; j++)
+	{	
+		if(S[Data[j]])
+		{
+			count++;
+		}			
+	}
+	std::cout<<count<<'\n';
+};
+
+int main(int argc, char* argv[])
+{
+	if(argv[1] == nullptr || argc%2 == 0)
+	{
+		return -1;
+	}
+	else
+	{
+		std::vector<int> S = sieve_Er();
+		for (int i = 1; i < argc - 1; i += 2)
 		{
 			int res = 0;
-			int u = V[i];
-			int v = V[i+1];
-			int j = 0;
-			while(Data[j] < u)
-			{
-				j++;
-			}
-			if(Data[j] != u)
-			{
-				j++;
-			}
-			for(j; Data[j] <= v; j++)
-			{	
-				if(a[Data[j]])
-				{
-					res++;
-				}			
-			}
-			std::cout<<res<<'\n';		
+			int a = std::atoi(argv[i]);
+			int b = std::atoi(argv[i + 1]);
+			prime_count(S, a, b);
 		}
 	}
 	return 0;
